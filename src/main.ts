a2c3d93f8c7c0404030ae07e968cf21fa82ac7c6
Ashutosh Dwivedi <ashutosh.dwivedi@servicenow.com>
2018-11-22 16:51:13 +0530

    trying to make github ui work

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { $ } from 'protractor';
 if (environment.production) {
   enableProdMode();
   Office.initialize = function () {
@@ -10,7 +11,7 @@ if (environment.production) {
     .catch(err => console.error(err));
   };
 }
-
+$('document').ready(() => {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
-
+});  
