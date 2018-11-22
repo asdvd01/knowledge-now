6936499e0183af5aa2947cd7557d06876feb1df2
Ashutosh Dwivedi <ashutosh.dwivedi@servicenow.com>
2018-11-22 12:02:08 +0530

    Adding authentication to get request

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http:HttpClient) {
-    
-   }
+  constructor(private http:HttpClient, private httpOptions : any) {
+     httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + btoa('admin' + ':' + '9235785668#Ac'),
+        'Accept': 'application/json'
+      })
+    };
+   // client.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //client.setRequestHeader('Access-Control-Allow-Methods', 'POST, PUT');           
+  }
    getUsers(){
      return this.http.get('https://jsonplaceholder.typicode.com/users');
    }
@@ -15,6 +24,6 @@ export class DataService {
       return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
    }
    getPosts(){
-    return this.http.get('https://ashutoshdwivedi.service-now.com/api/now/table/kb_knowledge');
+    return this.http.get('https://ashutoshdwivedi.service-now.com/api/now/table/kb_knowledge',this.httpOptions);
   }
 }
